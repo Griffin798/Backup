c7988e353755e66db5ca914f934fc7bd67e59160
somarkn99 <kesen.somar.99@gmail.com>
2021-12-25 13:37:42 +0200

    Build: SearchComponents

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import axios from 'axios';
 import './App.css';
 import Header from './components/UI/Header';
 import CharacterGrid from './components/charchters/CharacterGrid';
+import Search from './components/UI/Search';
 
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`http://hp-api.herokuapp.com/api/characters`)
-      console.log(result.data)
+      const result = await axios(`http://hp-api.herokuapp.com/api/characters?${query}`)
+
       setItems(result.data)
       setIsLoading(false)
     }
     fetchItems()
-  }, []) 
+  }, [query]) 
 
   return    <div className="container"> 
     <Header/>
+    <Search getQuery={(q) => setQuery(q) } />
     <CharacterGrid isLoading={isLoading} items= {items} />
    </div>
 }
