28cb6f7bac0bccb07519e24c96f2180c63225527
somarkn99 <kesen.somar.99@gmail.com>
2021-12-25 12:41:42 +0200

    Fetch Data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React, {useState , useEffect} from 'react'
+import axios from 'axios';
 import './App.css';
+import Header from './components/UI/Header';
 
 const App = () => {
-  return    <div className="container"> Harry </div>
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`http://hp-api.herokuapp.com/api/characters`)
+      console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  }, []) 
+
+  return    <div className="container"> 
+    <Header/>
+   </div>
 }
 
 export default App;
